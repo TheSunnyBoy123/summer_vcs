2ff3ecf585964f1f8102ab1de627d0293baff01b
Sunraj <sunrajpatelcph@gmail.com>
2024-05-31 02:34:48 +0530

    added documentation

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# summer_vcs
+# SOL VCS
+
+# Commands
+
+## `init`
+
+## Synopsis
+
+<pre>
+<b>sol init</b> [-w|--warning] [-v|--verbose] [-h|--help] <b>[directory]</b>
+</pre>
+
+## Description
+Initializes a sol repository in the given directory.
+
+
--- sol/cmd/init.go
+++ sol/cmd/init.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
@@ -10,31 +6,43 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var Directory string
+var targetFiles []string
+
+
+
 // initCmd represents the init command
 var initCmd = &cobra.Command{
-	Use:   "init",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init called")
+	Use:   "init <directory>",
+	Short: "Initialises sol within the provided directory",
+	Long: "Creates .sol directory as well as the metadata files required for sol to function properly.",
+	Args: cobra.MinimumNArgs(1),
+	RunE: func(_ *cobra.Command, _ []string) error {
+		return runInitCommand()
 	},
 }
 
-func init() {
-	rootCmd.AddCommand(initCmd)
 
-	// Here you will define your flags and configuration settings.
+func runInitCommand() error {
+	if exists, err := checkPathExists(vxRootDirName); err != nil {
+		return err
+	} else if exists {
+		return errors.New("vx root directory is already exist")
+	}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
+	if err := createDirectories(vxRootDirName, vxCommitDirPath, vxCheckoutDirPath); err != nil {
+		return err
+	}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	if err := createFile(vxStatusFilePath); err != nil {
+		return err
+	}
+
+	if err := createFile(vxStagingFilePath); err != nil {
+		return err
+	}
+
+	color.Green("All files are initialized within .vx/ directory!")
+
+	return nil
 }
